e951854c43595af0254a9b6c47ffd470182cb409
MichaelOtienno <michealvenum007@gmail.com>
2023-11-17 12:03:10 +0300

    coffee roasters

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,39 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingComponent } from './landing/landing.component';
+import { AboutComponent } from './about/about.component';
+import { PlanComponent } from './plan/plan.component';
+import { RouterModule, Routes } from '@angular/router';
+import { FooterComponent } from './footer/footer.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
+import { NavbarComponent } from './navbar/navbar.component';
+
+
+const routes: Routes = [
+
+  { path: 'landing', component: LandingComponent },
+  {path:'footer',component:FooterComponent},
+  {path:'plan',component:PlanComponent},
+  { path: 'about', component: AboutComponent },
+  { path: '', redirectTo: '/landing', pathMatch: 'full' },
+  { path: '**', component: PagenotfoundComponent },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    AboutComponent,
+    PlanComponent,
+    FooterComponent,
+    PagenotfoundComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
